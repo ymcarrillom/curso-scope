016dfaa0bb51ab6a87f5123c057f816b709817aa
yonatan <carri_2286@hotmail.com>
2025-10-05 19:53:38 -0500

    function.js spoce

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting () {
+    let userName = "Ana";
+    console.log(userName);
+
+    if (userName === "Ana") {
+        console.log(`hello ${userName}`)
+    }
+
+}
+
+greeting();
+console.log(userName)
